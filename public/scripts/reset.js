f31cf0e8c026577e7103ab7eca592f1bb3ec34e9
Rory Hackney <roryhackney@gmail.com>
2024-11-07 12:10:12 -0800

    moving static files into public

---
+++ public/scripts/reset.js
@@ -0,0 +1,16 @@
+"use strict";
+(function() {
+    window.addEventListener("load", init);
+
+    function init() {
+        const FORM = document.querySelector("form");
+        FORM.addEventListener("submit", (event) => {handleSubmit(event)});
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let span = document.querySelector("form span");
+        span.innerText = "Just kidding, you can't reset your password. Try again later, " +
+        "maybe I'll implement this eventually.";
+    }
+})();
